bf521e455a03b38efdb50cd11fd0e91835e5d6ac
izzetcosar <izzetcosar@gmail.com>
2024-11-24 16:48:41 +0300

    Add files via upload

--- SQLodev12.sql
+++ SQLodev12.sql
@@ -18,11 +18,14 @@ WHERE replacement_cost =
 
 
 
-SELECT customer.customer_id,customer.first_name ,customer.last_name,SUM(amount) 
+SELECT 
+    customer.customer_id,
+    customer.first_name ,customer.last_name,
+    COUNT(*) 
 FROM customer 
 JOIN payment  ON customer.customer_id = payment.customer_id
 GROUP BY customer.customer_id, customer.first_name, customer.last_name
-ORDER BY SUM(amount) DESC;
+ORDER BY COUNT(*)  DESC;
 
 
 
